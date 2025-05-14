c75d472c3a7922cc80e02fb507ff1f2ceeaa226e
atique-shahriar <satique06@gmail.com>
2025-05-15 01:00:24 +0600

    Add fade in animation

--- script.js
+++ script.js
@@ -161,3 +161,23 @@ titles.forEach((title) => {
 
 const title = document.querySelector(".section-title");
 if (title) observer.observe(title);
+
+const faders = document.querySelectorAll(".fade-in");
+
+const appearOnScroll = new IntersectionObserver(
+    (entries, observer) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+                observer.unobserve(entry.target);
+            }
+        });
+    },
+    {
+        threshold: 0.1,
+    }
+);
+
+faders.forEach((el) => {
+    appearOnScroll.observe(el);
+});
