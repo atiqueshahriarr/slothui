a59a26fa8e804b93facd7167ee9f6129c1c2e8a3
atique-shahriar <satique06@gmail.com>
2025-05-14 20:01:07 +0600

    Add counter in work section and create navbar fix, add shadow

--- script.js
+++ script.js
@@ -38,3 +38,93 @@ faqItems.forEach((item) => {
         }
     });
 });
+
+function startDataCounter() {
+    const dataElement = document.getElementById("data-efficiency-data");
+    const targetForData = 420;
+    const duration = 4000;
+    const frameRate = 1000 / 60;
+    const totalFrames = duration / frameRate;
+    const incrementForData = targetForData / totalFrames;
+
+    let countData = 0;
+
+    const intervalForData = setInterval(() => {
+        countData += incrementForData;
+        if (countData >= targetForData) {
+            countData = targetForData;
+            clearInterval(intervalForData);
+        }
+        dataElement.textContent = Math.floor(countData);
+    }, frameRate);
+}
+
+function startConversionCounter() {
+    const conversionElement = document.getElementById("conversion-rate-data");
+    const targetForConversion = 708;
+    const duration = 4000;
+    const frameRate = 1000 / 60;
+    const totalFrames = duration / frameRate;
+    const incrementForConversion = targetForConversion / totalFrames;
+
+    let countConversion = 0;
+
+    const intervalForConversion = setInterval(() => {
+        countConversion += incrementForConversion;
+        if (countConversion >= targetForConversion) {
+            countConversion = targetForConversion;
+            clearInterval(intervalForConversion);
+        }
+        conversionElement.textContent = Math.floor(countConversion);
+    }, frameRate);
+}
+
+function startLLMCounter() {
+    const llmElement = document.getElementById("llm-scale-data");
+    const targetForLLM = 1.8;
+    const duration = 4000;
+    const frameRate = 1000 / 60;
+    const totalFrames = duration / frameRate;
+    const incrementForLLM = targetForLLM / totalFrames;
+
+    let countLLM = 0;
+
+    const intervalForLLM = setInterval(() => {
+        countLLM += incrementForLLM;
+        if (countLLM >= targetForLLM) {
+            countLLM = targetForLLM;
+            clearInterval(intervalForLLM);
+        }
+        llmElement.textContent = countLLM.toFixed(1);
+    }, frameRate);
+}
+
+let dataCountStarted = false;
+let conversionCountStarted = false;
+let llmCountStarted = false;
+
+window.addEventListener("scroll", () => {
+    const dataEfficiencyelement = document.getElementById("data-efficiency-data");
+    const dataPosition = dataEfficiencyelement.getBoundingClientRect();
+
+    if (!dataCountStarted && dataPosition.top < window.innerHeight && dataPosition.bottom >= 0) {
+        dataCountStarted = true;
+        startDataCounter();
+    }
+
+    const conversionElement = document.getElementById("conversion-rate-data");
+    const conversionPosition = conversionElement.getBoundingClientRect();
+
+    if (!conversionCountStarted && conversionPosition.top < window.innerHeight && conversionPosition.bottom >= 0) {
+        conversionCountStarted = true;
+        startConversionCounter();
+    }
+
+    const llmElement = document.getElementById("llm-scale-data");
+    const llmPosition = llmElement.getBoundingClientRect();
+
+    if (!llmCountStarted && llmPosition.top < window.innerHeight && llmPosition.bottom >= 0) {
+        llmCountStarted = true;
+        startLLMCounter();
+    }
+});
